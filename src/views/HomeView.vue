5caa85e7d98198e6303bdc7de413998822ce782a
Martin Vadseth Høiby <mvhoiby@gmail.com>
2024-11-20 10:32:49 +0100

    Added Tailwind and DaisyUI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <h1>Home</h1>
-    <p>This is my first ever Vue project! It's pretty cool, looking forward to play around with it.</p>
+    <h1 className="text-3xl font-bold underline">Home</h1>
+    <span class="loading loading-spinner text-primary"></span>
+    <p>
+      This is my first ever Vue project! It's pretty cool, looking forward to
+      play around with it.
+    </p>
   </main>
 </template>
