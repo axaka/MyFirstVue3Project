4059c7fab3ea8b30ad79a90893778feba9c69ce0
Martin Vadseth Høiby <mvhoiby@gmail.com>
2024-11-20 10:02:54 +0100

    Vue is now running in a basic manner

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</routerLink>
+        <RouterLink to="/about">About</routerLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script>
+  import { RouterLink, RouterView } from "vue-router";
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <p>This is my first ever Vue project! It's pretty cool, looking forward to play around with it.</p>
+  </main>
+</template>
